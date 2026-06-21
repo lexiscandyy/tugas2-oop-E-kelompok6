919bd219e4751d6db85cc9141d089d5c9eb49c93
andikaseptianantha <andikaanjay791@gmail.com>
2026-06-21 11:07:33 +0800

    adding method

--- project/src/repository/UserRepository.java
+++ project/src/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
 public class UserRepository {
+    public void addUser(User user){}
+    public User getUserId(String id){}
+    public List<User> getAllUsers(){}
+    public void updateUser(User user){}
+    public void deleteUser(User user){}
 }
--- project/src/repository/VenueRepository.java
+++ project/src/repository/VenueRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
 public class VenueRepository {
+    public void addVenue(Venue venue){}
+    public User getVenueId(String id){}
+    public List<Venue> getAllVenue(){}
+    public void updateVenue(Venue venue){}
+    public void deleteVenue(Venue venue){}
 }
