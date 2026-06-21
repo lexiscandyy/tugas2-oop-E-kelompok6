1005a8fc72c14a4ef0ccd0e3e7934121ec1e1055
andikaseptianantha <andikaanjay791@gmail.com>
2026-06-21 11:00:34 +0800

    adding getter & setter

--- project/src/model/Venue.java
+++ project/src/model/Venue.java
@@ -5,4 +5,39 @@ public class Venue {
     private String name;
     private String address;
     private int maxCapacity;
+
+    public venue(String id, String name, String address, int maxCapacity){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getMaxCapacity(){
+        return maxCapacity;
+    }
+    public void setMaxCapacity(int maxCapacity){
+        this.maxCapacity = maxCapacity;
+    }
 }
