a96e9e542ee4e01c120208da0fbea7920b555aa3
ra** in**** <rama.id0174@gmail.com>
2026-06-22 23:49:22 +0800

    GETTER TICKET.java

--- project/src/model/Ticket.java
+++ project/src/model/Ticket.java
@@ -8,4 +8,32 @@ public class Ticket {
     private String totalPrice;
     private String purchaseDate;
     private String status;
+
+    public String getId(){
+        return id;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public String getUnitPrice(){
+        return unitPrice;
+    }
+
+    public String getTotalPrice(){
+        return totalPrice;
+    }
+
+    public String getPurchaseDate(){
+        return purchaseDate;
+    }
+
+    public String getStatus(){
+        return status;
+    }
 }
