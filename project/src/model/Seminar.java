d7782a4b6eb46126109291cf9ba3852c643dd82b
Lucifer <jayakomang81@gmail.com>
2026-06-23 22:00:55 +0800

    Update model seminar

--- project/src/model/Seminar.java
+++ project/src/model/Seminar.java
@@ -1,15 +1,28 @@
 package model;
 
-public class Seminar extends Event implements Refundable{
+public class Seminar extends Event implements Refundable {
 
-    @Override
-    public double calculateRefund(int daysBeforeEvent) {
-        // rumus dan aturan refund
-        return .00;
+    public Seminar() {}
+
+    public Seminar(String id, String name, String venueId, String organizerId,
+                   String date, double basePrice, String createdAt) {
+        super(id, "seminar", name, venueId, organizerId, date, basePrice, createdAt);
     }
 
     @Override
     public double calculateTicketPrice(String category) {
-        return 0;
+        // Flat price, semua kategori sama
+        return getBasePrice();
+    }
+
+    @Override
+    public double calculateRefund(int daysBeforeEvent) {
+        if (daysBeforeEvent > 1) return 1.0;  // 100%
+        else                     return 0.0;  // 0%
+    }
+
+    @Override
+    public boolean isRefundable() {
+        return true;
     }
 }
