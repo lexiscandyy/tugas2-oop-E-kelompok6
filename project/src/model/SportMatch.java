f55277fdc283cabe6d341edffef7fb3a2b7be2b5
Lucifer <jayakomang81@gmail.com>
2026-06-23 22:02:12 +0800

    Update model

--- project/src/model/SportMatch.java
+++ project/src/model/SportMatch.java
@@ -1,9 +1,22 @@
 package model;
 
-public class SportMatch extends Event{
+public class SportMatch extends Event {
+
+    public SportMatch() {}
+
+    public SportMatch(String id, String name, String venueId, String organizerId,
+                      String date, double basePrice, String createdAt) {
+        super(id, "sport_match", name, venueId, organizerId, date, basePrice, createdAt);
+    }
 
     @Override
     public double calculateTicketPrice(String category) {
-        return 0;
+        switch (category.toLowerCase()) {
+            case "vip":  return getBasePrice() * 2.5;
+            case "vvip": return getBasePrice() * 5.0;
+            case "tribune":
+            default:     return getBasePrice() * 1.0;
+        }
     }
+
 }
