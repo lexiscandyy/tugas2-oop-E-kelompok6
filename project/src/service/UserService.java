92dc344eee279f7b779b1dda494ab65540da5686
andikaseptianantha <andikaanjay791@gmail.com>
2026-06-23 19:20:02 +0800

    Service

--- project/src/service/UserService.java
+++ project/src/service/UserService.java
@@ -1,4 +1,34 @@
 package service;
 
+import model.User;
+import repository.UserRepository;
+
+import java.util.List;
+
 public class UserService {
+    private UserRepository userRepository;
+
+    public userService(){
+        userRepository = new UserRepository();
+    }
+
+    public void addUser(User user){
+        userRepository.addUser(user);
+    }
+
+    public User getUserId(String id){
+        return userRepository.getUserId(id);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.getAllUsers();
+    }
+
+    public void updateUser (User user){
+        userRepository.updateUser(user);
+    }
+
+    public void deleteUser(String id){
+        userRepository.deleteUser(id);
+    }
 }
--- project/src/service/VenueService.java
+++ project/src/service/VenueService.java
@@ -1,4 +1,34 @@
 package service;
 
+import model.Venue;
+import repository.VenueRepository;
+
+import java.util.List;
+
 public class VenueService {
+    private VenueRepository venueRepository;
+
+    public venueService(){
+        venueRepository = new VenueRepository();
+    }
+
+    public void addVenue(Venue venue){
+        venueRepository.addVenue(venue);
+    }
+
+    public Venue getVenueId(String id){
+        return venueRepository.getVenueId(id);
+    }
+
+    public List<Venue> getAllVenues(){
+        return venueRepository.getAllVenues();
+    }
+
+    public void updateVenue (Venue venue){
+        venueRepository.updateVenue(venue);
+    }
+
+    public void deleteVenue(String id){
+        venueRepository.deleteVenue(id);
+    }
 }
