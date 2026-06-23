b136171c74dab961e6d632e6e0167d53fd6d934f
ra** in**** <rama.id0174@gmail.com>
2026-06-23 23:59:09 +0800

    INSERT EVENT REPO

--- project/src/repository/EventRepository.java
+++ project/src/repository/EventRepository.java
@@ -1,4 +1,28 @@
 package repository;
+import database.DatabaseManager;
+import model.Event;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class EventRepository {
+
+    public void insert(Event event) throws SQLException {
+        String query = "insert into events (id, type, name, venue_id, organizer_id, date, base_price, created_at) " +
+                "values (?,?,?,?,?,?,?,?)";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setString(1, event.getId());
+            ps.setString(2, event.getType());
+            ps.setString(3, event.getName());
+            ps.setString(4, event.getVenueId());
+            ps.setString(5, event.getOrganizerId());
+            ps.setString(6, event.getDate());
+            ps.setString(7, toString(event.getBasePrice()));
+            ps.setString(8, event.getCreatedAt());
+        }
+    }
+
 }
--- project/src/repository/TicketRepository.java
+++ project/src/repository/TicketRepository.java
@@ -1,4 +1,29 @@
 package repository;
+import database.DatabaseManager;
+import model.Ticket;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class TicketRepository {
+
+    public void insert(Ticket ticket) throws SQLException {
+        String query = "insert into ticket (id, event_id, user_id, category, quantity, unit_price, total_price)" +
+                " values (?, ?, ?, ?, ?, ?, ?";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setString(1, ticket.getId());
+            ps.setString(2, ticket.getEventId());
+            ps.setString(3, ticket.getUserId());
+            ps.setString(4, ticket.getCategory());
+            ps.setInt(5,    ticket.getQuantity());
+            ps.setDouble(6, ticket.getUnitPrice());
+            ps.setDouble(7, ticket.getTotalPrice());
+            ps.executeUpdate();
+        }
+    }
+
 }
